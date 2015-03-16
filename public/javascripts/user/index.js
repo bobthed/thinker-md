f36b821f279cf0d45f09f223b7c4d1ac8bad590f
ling <magic.ling@live.cn>
2015-03-16 19:17:26 +0800

    插入IMAGE Bug

--- public/javascripts/user/index.js
+++ public/javascripts/user/index.js
@@ -2,11 +2,24 @@
  * Created by ling on 2015/3/2.
  */
 
-$("textarea[data-provide='markdown']").markdown({
+$("#md").markdown({
     language: 'zh',
     fullscreen: {
         enable: true
     },
+    resize:'vertical',
+    localStorage:'md',
+    imgurl: 'http://192.168.1.142:8080/upload',
+    base64url: 'http://192.168.1.142:8080/base64'
+});
+
+$("#md2").markdown({
+    language: 'zh',
+    fullscreen: {
+        enable: true
+    },
+    resize:'vertical',
+    localStorage:'md2',
     imgurl: 'http://192.168.1.142:8080/upload',
     base64url: 'http://192.168.1.142:8080/base64'
 });
