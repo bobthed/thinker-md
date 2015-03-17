6522432d903c99f04d0ac35833bd98a1392b70e4
ling <magic.ling@live.cn>
2015-03-17 13:59:17 +0800

    添加撤消和恢复功能

--- public/javascripts/vendor/markdown/tab.js
+++ public/javascripts/vendor/markdown/tab.js
@@ -63,14 +63,33 @@ String.prototype.countOfTabInCloseTag = function () {
     return (this.match(reg) || []).length;
 };
 
+var textareaMap = {};
+var EditCommand = Undo.Command.extend({
+    constructor: function (textarea, oldValue, newValue) {
+        this.textarea = textarea;
+        this.oldValue = oldValue;
+        this.newValue = newValue;
+    },
+    execute: function () {
+    },
+    undo: function () {
+        this.textarea.value = this.oldValue;
+    },
+    redo: function () {
+        this.textarea.value = this.newValue;
+    }
+});
 
-window.onkeydown = function (evt) {
+var tabFunc = function (evt) {
     evt = evt || window.event;
     var keyCode = evt.keyCode,
         tab = 9,
-        enter = 13;
-
+        enter = 13,
+        key_y = 89,
+        key_z = 90;
     var target = evt.target,
+        stack = null,
+        dataStep = null,
         selectionStart = -1,
         selectionEnd = -1,
         tabKey = '\u0020\u0020\u0020\u0020',
@@ -80,11 +99,21 @@ window.onkeydown = function (evt) {
         prefix = '',
         suffix = '';
     if (target && target.tagName === 'TEXTAREA') {
+        if (null === target.getAttribute('data-step')) {
+            target.setAttribute('data-step', new Date().getTime().toString());
+        }
+        dataStep = target.getAttribute('data-step');
+        if (undefined === textareaMap[dataStep]) {
+            stack = new Undo.Stack();
+            textareaMap[dataStep] = stack;
+        } else {
+            stack = textareaMap[dataStep];
+        }
         selectionStart = target.selectionStart;
         selectionEnd = target.selectionEnd;
         value = target.value;
         if (selectionStart < 0 || selectionEnd < 0) {
-            return;
+            return stopEvent(evt);
         } else {
             prefix = value.substring(0, selectionStart);
             suffix = value.substring(selectionEnd);
@@ -93,59 +122,76 @@ window.onkeydown = function (evt) {
         return;
     }
 
-
     //tab
     if (keyCode === tab) {
-        value = prefix + tabKey + suffix;
+        var _value = prefix + tabKey + suffix;
 
         selectionStart += 4;
         selectionEnd = selectionStart;
-        target.value = value;
+        target.value = _value;
         target.setSelectionRange(selectionStart, selectionEnd);
+        stack.execute(new EditCommand(target, value, _value));
         return stopEvent(evt);
     }
 
     //enter
     if (keyCode === enter) {
         //{}
-        var frist = prefix.trim().lastChar(),
+        var _value = '',
+            frist = prefix.trim().lastChar(),
             last = suffix.trim().fristChar(),
             count = prefix.countOf(/\u000a/g);
         if (('\u003b' === frist || '\u0029' === frist || '\u007b' === frist) && '\u007d' === last) {
             if (count === 0) {
-                value = prefix + enterKey + tabKey + enterKey + suffix;
+                _value = prefix + enterKey + tabKey + enterKey + suffix;
                 selectionStart += 5;
             } else if (count > 0) {
                 var tabs = prefix.substring(prefix.lastIndexOf('\u000a'), selectionStart).countOfTab(), i = 0, tabStr = '';
                 for (; i < tabs; ++i) {
                     tabStr += tabKey;
                 }
-                value = '';
-                value += prefix;
-                value += enterKey;
-                value += tabStr;
+                _value += prefix;
+                _value += enterKey;
+                _value += tabStr;
                 if ('\u003b' !== frist) {
-                    value += tabKey;
+                    _value += tabKey;
                     ++tabs;
                 }
                 if (enterKey !== suffix.fristChar()) {
-                    value += enterKey;
-                    value += tabStr;
+                    _value += enterKey;
+                    _value += tabStr;
                 }
-                value += suffix;
+                _value += suffix;
 
                 selectionStart += 1 + (tabs * 4);
             }
         } else {
-            value = prefix + enterKey + suffix;
+            _value = prefix + enterKey + suffix;
             ++selectionStart;
         }
 
         selectionEnd = selectionStart;
-        target.value = value;
+        target.value = _value;
         target.setSelectionRange(selectionStart, selectionEnd);
-
+        stack.execute(new EditCommand(target, value, _value));
         return stopEvent(evt);
     }
 
+    //Ctrl+Z
+    if (evt.ctrlKey === true && keyCode === key_z) {
+        if (stack.canUndo()) {
+            stack.undo();
+        }
+        return stopEvent(evt);
+    }
+
+    //Ctrl+y
+    if (evt.ctrlKey === true && keyCode === key_y) {
+        if (stack.canRedo()) {
+            stack.redo();
+        }
+        return stopEvent(evt);
+    }
 };
+
+window.document.addEventListener('keydown', tabFunc, false);
